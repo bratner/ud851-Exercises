10833e358ad569f5fae0cf518c135526cca879c4
Boris Ratner <ratner2@gmail.com>
2018-01-29 03:41:08 +0200

    doing lesson 12

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/MainActivity.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/MainActivity.java
@@ -15,15 +15,20 @@
  */
 package com.example.android.background;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.android.background.sync.WaterReminderIntentService;
 import com.example.android.background.utilities.PreferenceUtilities;
+import com.example.android.background.sync.ReminderTasks;
 
 public class MainActivity extends AppCompatActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener {
@@ -79,9 +84,13 @@ public class MainActivity extends AppCompatActivity implements
         if (mToast != null) mToast.cancel();
         mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
         mToast.show();
-        // TODO (15) Create an explicit intent for WaterReminderIntentService
-        // TODO (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
-        // TODO (17) Call startService and pass the explicit intent you just created
+        // DONE (15) Create an explicit intent for WaterReminderIntentService
+        Intent increaseWaterIntent = new Intent(this, WaterReminderIntentService.class);
+        // DONE (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
+        increaseWaterIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+        // DONE (17) Call startService and pass the explicit intent you just created
+        Log.d("BRAT", "Starting the service");
+        startService(increaseWaterIntent);
     }
 
     @Override
--- Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
+++ Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
@@ -16,31 +16,65 @@
 package com.example.android.background.sync;
 
 
+import android.content.Context;
+
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
 public class ReminderUtilities {
-    // TODO (15) Create three constants and one variable:
+    // DONE (15) Create three constants and one variable:
     //  - REMINDER_INTERVAL_SECONDS should be an integer constant storing the number of seconds in 15 minutes
     //  - SYNC_FLEXTIME_SECONDS should also be an integer constant storing the number of seconds in 15 minutes
     //  - REMINDER_JOB_TAG should be a String constant, storing something like "hydration_reminder_tag"
     //  - sInitialized should be a private static boolean variable which will store whether the job
     //    has been activated or not
+    public static final int REMINDER_INTERVAL_SECONDS = 15*60;
+    public static final int SYNC_FLEXTIME_SECONDS = 15*60;
+    public static final String REMINDER_JOB_TAG = "hydration_reminder_jobtag";
+    private static boolean sInitialized = false;
 
-    // TODO (16) Create a synchronized, public static method called scheduleChargingReminder that takes
+    // DONE (16) Create a synchronized, public static method called scheduleChargingReminder that takes
     // in a context. This method will use FirebaseJobDispatcher to schedule a job that repeats roughly
     // every REMINDER_INTERVAL_SECONDS when the phone is charging. It will trigger WaterReminderFirebaseJobService
     // Checkout https://github.com/firebase/firebase-jobdispatcher-android for an example
-        // TODO (17) If the job has already been initialized, return
-        // TODO (18) Create a new GooglePlayDriver
-        // TODO (19) Create a new FirebaseJobDispatcher with the driver
-        // TODO (20) Use FirebaseJobDispatcher's newJobBuilder method to build a job which:
-            // - has WaterReminderFirebaseJobService as it's service
-            // - has the tag REMINDER_JOB_TAG
-            // - only triggers if the device is charging
-            // - has the lifetime of the job as forever
-            // - has the job recur
-            // - occurs every 15 minutes with a window of 15 minutes. You can do this using a
-            //   setTrigger, passing in a Trigger.executionWindow
-            // - replaces the current job if it's already running
+    public static synchronized void scheduleCharginReminder(Context context) {
+        if (sInitialized)
+            return;
+
+
+        // DONE (17) If the job has already been initialized, return
+
+        // DONE (18) Create a new GooglePlayDriver
+        GooglePlayDriver googlePlayDriver = new GooglePlayDriver(context);
+        // DONE (19) Create a new FirebaseJobDispatcher with the driver
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(googlePlayDriver);
+        // DONE (20) Use FirebaseJobDispatcher's newJobBuilder method to build a job which:
+        Job myJob = dispatcher.newJobBuilder()
+                // - has WaterReminderFirebaseJobService as it's service
+                .setService(WaterReminderFirebaseJobService.class)
+
+        // - has the tag REMINDER_JOB_TAG
+                .setTag(REMINDER_JOB_TAG)
+        // - only triggers if the device is charging
+                .setConstraints(Constraint.DEVICE_CHARGING)
+        // - has the lifetime of the job as forever
+                .setLifetime(Lifetime.FOREVER)
+        // - has the job recur
+                .setRecurring(true)
+        // - occurs every 15 minutes with a window of 15 minutes. You can do this using a
+        //   setTrigger, passing in a Trigger.executionWindow
+                .setTrigger(Trigger.executionWindow(REMINDER_INTERVAL_SECONDS, SYNC_FLEXTIME_SECONDS))
+        // - replaces the current job if it's already running
+                .setReplaceCurrent(true)
         // Finally, you should build the job.
-        // TODO (21) Use dispatcher's schedule method to schedule the job
-        // TODO (22) Set sInitialized to true to mark that we're done setting up the job
+                .build();
+        // DONE (21) Use dispatcher's schedule method to schedule the job
+        dispatcher.schedule(myJob);
+        // DONE (22) Set sInitialized to true to mark that we're done setting up the job
+        sInitialized = true;
+    }
 }
